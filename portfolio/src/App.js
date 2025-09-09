f51650caa633eba77364b2d400391c04c91e3c12
ANTONIO GUERRERO <167793825+Antoguerrero@users.noreply.github.com>
2025-09-09 19:37:29 +0200

    inicio de proyecto

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TONI'S PORTFOLIO</h1>
+    </div>
+  );
+}
+
+export default App;
